d7beea8d2ed02da9b368e13ae560bdd524f414df
alkavats1 <alka.vats@knoldus.com>
2021-09-07 20:05:57 +0530

    Template driven forms with user validations

---
+++ src/app/template-driven-form/template-driven-form.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-template-driven-form',
+  templateUrl: './template-driven-form.component.html',
+  styleUrls: ['./template-driven-form.component.scss']
+})
+export class TemplateDrivenFormComponent implements OnInit {
+
+  model = {
+    firstName: '',
+    lastName: '',
+    email: '',
+    password: ''
+  };
+
+  onSubmit() {
+    alert('SUCCESS!! :-)\n\n' + JSON.stringify(this.model));
+  }
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
